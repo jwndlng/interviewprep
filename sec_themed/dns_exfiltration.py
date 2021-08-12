b3c8d56d44a50b397b1d6651a54bcdf190a217af
jwendling <jan.wendling@gmail.com>
2021-08-12 21:52:09 +0200

    Filename refactoring

---
+++ sec_themed/dns_exfiltration.py
@@ -0,0 +1,13 @@
+"""
+# DNS Exfiltration
+
+The idea of this implementation is to simulate and show how a DNS exfiltration could work
+
+## Components
+
+- Client
+  - Simulating a client application running on a compromised system and sending data
+- Server
+  - Receiving requests from the client and re-assembling the encoded message
+
+"""
